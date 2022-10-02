67cbdf1d227bc0c5270fde7b1cf20bcb5fa9f0bf
ecoricemon <ecoricemon@gmail.com>
2022-10-02 22:52:13 +0900

    refactor: Seperate harbor from cluster

---
+++ ansible/ext.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "Usage: ./init.sh type"
+    echo "type: vbox | utm"
+	exit 0
+fi
+
+type=$1
+
+if [ $type = "vbox" ]; then
+	for remote in 10.0.3.9; do
+		ssh-copy-id -o ProxyJump="kube@localhost:8022" kube@$remote 2>/dev/null
+	done
+	ansible-playbook -i inventories/inv-vbox.yaml -K playbooks/ext.yaml
+elif [ $type = "utm" ]; then
+	for remote in 192.168.128.9; do
+		ssh-copy-id kube@$remote 2>/dev/null
+	done
+	ansible-playbook -i inventories/inv-utm.yaml -K playbooks/ext.yaml
+fi
+
